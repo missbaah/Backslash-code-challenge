230c98f48e5d445d6ac123e28ff36d1061a22baf
Adwoa Addo-Brako <baahbrako@gmail.com>
2023-03-29 12:27:40 +0000

    rending products in store

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { products } from "../data.jsx";
 
 const Store = () => {
-  return <div>Store</div>;
+  const ListofProducts = products.map((product) => {
+    return (
+      <div key={product.id}>
+        <img src={product.thumbnail} alt={product.title} />
+        <section className="product-info">
+          <div>
+            <h4>{product.title}</h4>
+            <p> ₵ {product.price}</p>
+          </div>
+          <button>Add to Cart</button>
+        </section>
+      </div>
+    );
+  });
+
+  return (
+    <main>
+      <h1>Shop the lastest items</h1>
+      <section className="grid-container">{ListofProducts}</section>
+    </main>
+  );
 };
 
 export default Store;
