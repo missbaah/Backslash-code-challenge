ed42a691136068d2c4081e4a1fa04c87a298c677
Adwoa Addo-Brako <baahbrako@gmail.com>
2023-03-29 11:52:05 +0000

    add dummy data in data.jsx

---
+++ src/data.jsx
@@ -0,0 +1,98 @@
+export const products = [
+  {
+    id: 1,
+    title: "Basic Black Tee",
+    price: 50,
+    category: "clothes",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+  },
+  {
+    id: 2,
+    title: "Mechanical Pencil",
+    price: 8,
+    category: "stationary",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+  },
+  {
+    id: 3,
+    title: "Tumbler",
+    price: 80,
+    category: "drinkware",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+  },
+  {
+    id: 4,
+    title: "Phone Holder",
+    price: 35,
+    category: "organisers",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/category-page-05-image-card-04.jpg",
+  },
+  {
+    id: 5,
+    title: "Refill Multi-Packs",
+    price: 20,
+    category: "stationary",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-04.jpg",
+  },
+  {
+    id: 6,
+    title: "Sticky Note Holder",
+    price: 25,
+    category: "organisers",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/category-page-03-image-card-04.jpg",
+  },
+  {
+    id: 7,
+    title: "Basic Nude Tee",
+    price: 50,
+    category: "clothes",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg",
+  },
+  {
+    id: 8,
+    title: "Earthen Bottle",
+    price: 50,
+    category: "drinkware",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+  },
+  {
+    id: 9,
+    title: "Pen Holders",
+    price: 50,
+    category: "organisers",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/category-page-05-image-card-02.jpg",
+  },
+  {
+    id: 10,
+    title: "Scissors",
+    price: 12,
+    category: "stationary",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-07.jpg",
+  },
+  {
+    id: 11,
+    title: "Glass Water Bottle",
+    price: 150,
+    category: "drinkware",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/category-page-05-image-card-07.jpg",
+  },
+  {
+    id: 12,
+    title: "Polkadots Tee",
+    price: 65,
+    category: "clothes",
+    thumbnail:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg",
+  },
+];
