7ddabad4dc391c578488314afd7f6b6612b83d1f
Adwoa Addo-Brako <baahbrako@gmail.com>
2023-03-29 11:16:21 +0000

    update Navbar.jsx and style in App.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/cart">Cart</Link>
+      <Link to="/cart">
+        {" "}
+        <FontAwesomeIcon icon={faCartShopping} /> Cart
+      </Link>
     </nav>
   );
 };
